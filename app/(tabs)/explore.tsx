40ffbe8a840539916a694f6d6130d97830a1f7e6
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-02 11:11:29 -0300

    chore: estruturação do projeto

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,11 @@
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+      <Text > Homeeeee </Text>
+    </View>
+  );
+}
+
