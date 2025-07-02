3ecddf3498c557f31cc1ebd6baf9a415528fd47b
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-02 19:15:24 -0300

    feat: cricao do componente selecionador

---
+++ components/selecionador/style.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, ViewStyle } from 'react-native';
+
+
+const baseBox: ViewStyle = {
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 4,
+    padding: 8,
+};
+
+export const styles = StyleSheet.create({
+    container: {
+        marginBottom: 20,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginBottom: 8,
+    },
+    grid: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: 12,
+    },
+    itemBox: {
+        ...baseBox,
+        width: 48,
+        height: 48,
+    },
+    itemBoxComDescricao: {
+        ...baseBox,
+        width: 55,
+        height: 55,
+    },
+    label: {
+        fontSize: 12,
+        marginTop: 4,
+        color: '#374151',
+        textAlign: 'center',
+    },
+});
