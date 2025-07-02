924873463432d47bc8b4cf0e6d0b0617043c3cc8
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-02 14:31:23 -0300

    feat: criaçao de tabs personalizadas e configuração da rota

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,9 @@
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>
-        home
-      </Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hoome</Text>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
