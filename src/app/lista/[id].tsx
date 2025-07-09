e0bc3dbf711e462faf3685ed96e3c504c4bd988a
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-09 14:53:46 -0300

    feat: criação da pagina de exibição dos itens das listas

--- src/app/lista/[id].tsx
+++ src/app/lista/[id].tsx
@@ -1,32 +1,58 @@
-
 import BotaoFlutuante from "@/src/components/botao-flutuante";
+import CardItensLista from "@/src/components/card-itens-lista";
+import Modal from "@/src/components/modal";
+import { useBuscarListaPorId } from "@/src/hooks/useListas";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import React, { useCallback, useState } from "react";
+import { ActivityIndicator, SafeAreaView, Text } from "react-native";
 import ToastManager from "toastify-react-native/components/ToastManager";
+import { styles } from "./styles";
+import { COLORS } from "@/src/constants/Colors";
 
-
-export default function VisualizarListaToDo() {
+export default function VisualizarLista() {
     const router = useRouter();
-    const { id } = useLocalSearchParams();
+    const { id } = useLocalSearchParams<{ id: string }>();
+
+    const {
+        data: lista,
+        isPending,
+        isError,
+        error,
+        refetch,
+    } = useBuscarListaPorId(id);
+
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(async () => {
+        setRefreshing(true);
+        try {
+            await refetch();
+        } finally {
+            setRefreshing(false);
+        }
+    }, [refetch]);
 
     return (
         <SafeAreaView style={styles.containerPrincipal}>
-            <Text>
-                O ID dessa lista é {id}
-            </Text>
-            <ToastManager />
-            <BotaoFlutuante onPress={() => router.push("../adicionar-itens")} />
+            <>
+                <ToastManager />
+
+                <Modal title="Lista de Compras">
+                    {isPending && !refreshing ? (
+                        <ActivityIndicator size="large" color={COLORS.verde_principal} />
+                    ) : (
+                        <CardItensLista
+                            listaId={id}
+                            itensAgrupados={lista?.itensAgrupados}
+                            refreshing={refreshing}
+                            onRefresh={onRefresh}
+                        />)}
+                </Modal>
+
+                <BotaoFlutuante
+                    onPress={() => router.push(`/adicionar-itens?id=${id}`)}
+                />
+            </>
         </SafeAreaView>
     );
 }
-
-const styles = StyleSheet.create({
-    containerPrincipal: {
-        flex: 1,
-        padding: 16,
-    },
-    scrollContent: {
-        padding: 16
-    },
-});
