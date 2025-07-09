f7f868e502074d8afa945c05d0790e116df7a28e
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-09 14:51:25 -0300

    feat: alteração do componente modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,32 +1,27 @@
-import { stylesCentral } from '@/src/styles/stylesCentral';
-import { Ionicons } from '@expo/vector-icons';
-import { useRouter } from 'expo-router';
-import React from 'react';
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import { styles } from './styles';
+import { ModalProps } from "@/src/types/components/componentsTypes";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
 
-type ModalProps = {
-    title: string;
-    children: React.ReactNode;
-};
 
 export default function Modal({ title, children }: ModalProps) {
     const router = useRouter();
 
     return (
-    <ScrollView>
         <View style={[styles.modal]}>
             <View style={styles.header}>
                 <TouchableOpacity onPress={() => router.back()}>
                     <Ionicons name="arrow-back" size={24} color="#000" />
                 </TouchableOpacity>
-                <Text style={styles.title}>{title}</Text>
+                {typeof title === "string" ? (
+                    <Text style={styles.title}>{title}</Text>
+                ) : (
+                    title
+                )}
             </View>
-                <View style={styles.content}>{children}</View>
+            <View style={styles.content}>{children}</View>
         </View>
-            </ScrollView>
-
-
     );
 }
-
