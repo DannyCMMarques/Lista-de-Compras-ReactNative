8b1180789d5692750c387ef5541108308d667614
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-09 14:50:46 -0300

    feat: alteração do tituloIcone

--- src/components/ui/tituloIcone/index.tsx
+++ src/components/ui/tituloIcone/index.tsx
@@ -1,28 +1,23 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { Text, View, TouchableOpacity } from "react-native";
-import { styles } from "./styles";
 import { COLORS } from "@/src/constants/Colors";
+import { styles } from "./styles";
+import { TituloComIconeProps } from "@/src/types/components/componentsTypes";
 
-type TituloComIconeProps = {
-    iconName: keyof typeof MaterialIcons.glyphMap;
-    color?: string;
-    titulo: string;
-    isCard?: boolean;
-    subtitulo?: string;
-    onPress?: ()=> void
-};
+
 
 export default function TituloComIcone({
     iconName,
     color = COLORS.verde_principal,
     titulo,
     isCard = false,
-    subtitulo, 
+    subtitulo,
     onPress,
-}: TituloComIconeProps) {
+    comLinha = false,
+}: TituloComIconeProps ) {
     return (
-        <View style={[styles.wrapper]}>
-            <View style={styles.iconeTitulo}>
+        <View style={styles.wrapper}>
+            <View style={[styles.linhaHorizontalWrapper]}>
                 <View style={styles.iconLeft}>
                     <View
                         style={[
@@ -33,6 +28,7 @@ export default function TituloComIcone({
                     >
                         <MaterialIcons name={iconName} size={24} color={color} />
                     </View>
+
                     <View>
                         <Text style={styles.title}>{titulo}</Text>
                         {isCard && subtitulo && (
@@ -41,15 +37,14 @@ export default function TituloComIcone({
                     </View>
                 </View>
 
-                {isCard && (
-                    <View style={{ justifyContent: "center" }}>
-                        <TouchableOpacity onPress={onPress}>
-                            <MaterialIcons name="share" size={24} color="#9ca3af" />
-                        </TouchableOpacity>
-                    </View>
+                {isCard && !comLinha && (
+                    <TouchableOpacity onPress={onPress}>
+                        <MaterialIcons name="share" size={24} color="#9ca3af" />
+                    </TouchableOpacity>
                 )}
+
+                {comLinha && <View style={styles.linhaCategoria} />}
             </View>
         </View>
-
     );
 }
