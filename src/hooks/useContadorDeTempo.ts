8de4e886ae28eb0112fa299ee37801e88b282138
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-28 17:03:07 -0300

    feat:eliminação de números magicos

--- src/hooks/useContadorDeTempo.ts
+++ src/hooks/useContadorDeTempo.ts
@@ -1,3 +1,5 @@
+import { DIAS_POR_MES, MESES_POR_ANO, MILIS_POR_DIA, UM_ANO, UM_DIA, UM_MES } from "../utils/constants/indicadores_tempo";
+
 export const useContadorDeTempo = (data: Date | string): string => {
   const agora = new Date();
   const informada = new Date(data);
@@ -10,21 +12,21 @@ export const useContadorDeTempo = (data: Date | string): string => {
   );
 
   const diferencaEmMilissegundos = hoje.getTime() - diaInformado.getTime();
-  const diferencaEmDias = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24));
+  const diferencaEmDias = Math.floor(diferencaEmMilissegundos / (MILIS_POR_DIA));
 
   if (diferencaEmDias === 0) return "Hoje";
-  if (diferencaEmDias === 1) return "Ontem";
-  if (diferencaEmDias < 30) return `${diferencaEmDias} dias atrás`;
+  if (diferencaEmDias === UM_DIA) return "Ontem";
+  if (diferencaEmDias < DIAS_POR_MES) return `${diferencaEmDias} dias atrás`;
 
-  const diferencaEmMeses = Math.floor(diferencaEmDias / 30);
-  if (diferencaEmMeses < 12) {
-    return diferencaEmMeses === 1
+  const diferencaEmMeses = Math.floor(diferencaEmDias / DIAS_POR_MES);
+  if (diferencaEmMeses < MESES_POR_ANO) {
+    return diferencaEmMeses === UM_MES
       ? "1 mês atrás"
       : `${diferencaEmMeses} meses atrás`;
   }
 
-  const diferencaEmAnos = Math.floor(diferencaEmMeses / 12);
-  return diferencaEmAnos === 1
+  const diferencaEmAnos = Math.floor(diferencaEmMeses / MESES_POR_ANO);
+  return diferencaEmAnos === UM_ANO
     ? "1 ano atrás"
     : `${diferencaEmAnos} anos atrás`;
 };
