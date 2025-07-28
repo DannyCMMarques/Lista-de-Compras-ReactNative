773638f93a8083bd74dea6ea70d727ae6522f75b
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-28 15:32:36 -0300

    feat: colocado no env as credenciais da firebase, evitando que essas sejam expostas.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQYUOdVpHagr3WdXAw8blgyBi9oyk6J3U",
-  authDomain: "shopping-list-app-90827.firebaseapp.com",
-  projectId: "shopping-list-app-90827",
-  storageBucket: "shopping-list-app-90827.firebasestorage.app",
-  messagingSenderId: "169847957305",
-  appId: "1:169847957305:web:1df611d5abcb1543d7aae8",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
